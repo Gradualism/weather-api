11795da122371e80c6ddc627f7b19c9c00989957
Gradualism <43960086+Gradualism@users.noreply.github.com>
2018-10-08 13:29:45 -0400

    Add initial functions to JS
    For testing purposes, console.log the results of the API

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const appId = config.SECRET_KEY;
+const units = "imperial";
+let searchMethod = "zip";
 
+function searchWeather(searchTerm) {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod=searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+    return result.json();
+  }).then( result => {
+    init(result);
+  });
+}
+
+function init(resultFromServer) {
+  console.log(resultFromServer);
+}
