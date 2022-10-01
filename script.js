173ad142b52e73aa4fda7b3cd72471d2cbc9506c
S. Hall <shall.usnavy@gmail.com>
2022-10-01 19:30:22 -0400

    Add testing without framework

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const units = "imperial";
 let searchMethod = "zip";
 
 function searchWeather(searchTerm) {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod=searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod = searchTerm}&APPID=${appId}&units=${units}`).then(result => {
     return result.json();
-  }).then( result => {
+  }).then(result => {
     init(result);
   });
 }
@@ -13,3 +13,30 @@ function searchWeather(searchTerm) {
 function init(resultFromServer) {
   console.log(resultFromServer);
 }
+
+// Testing without a framework
+function it(description, fn) {
+  try {
+    fn();
+    console.log("\x1b[32m%s\x1b[0m", "\u2714 " + description)
+  } catch (error) {
+    console.log("\n \x1b[32m%s\x1b[0m", "\u2714 " + description)
+    console.error();
+  }
+}
+
+function assert(condition) {
+  if (!condition) {
+    throw new Error();
+  }
+}
+
+// Testing test with example
+function sum(a, b) {
+  return a + b;
+}
+
+// How to use the test
+it("should return a sum of two integers", function () {
+  assert(sum(2, 11) === 13);
+});
